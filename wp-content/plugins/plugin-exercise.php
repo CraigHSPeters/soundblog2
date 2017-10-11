9eaffaefbc1aa21c0802a21e23df5cbbed31eb3d
Craig Peters <cpeters1@northislandcollege.ca>
2017-10-11 10:30:55 -0700

    Initial Commit

---
+++ wp-content/plugins/plugin-exercise.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Simple Google Analytics Plugin
+Plugin URI:
+Description: Adds a Google analytics trascking code to the <head> of your theme, by hooking to wp_head.
+Author: Craig Peters
+Version: 1.0
+ */
+?>
+
+<?php
+function wpmudev_google_analytics() { ?>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107106386-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-107106386-2');
+</script>
+
+<?php }
+add_action( 'wp_head', 'wpmudev_google_analytics', 10 );
